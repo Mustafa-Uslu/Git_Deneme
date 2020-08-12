7c1faeacca5d03eb5902e4e5e08a119c4b7e6bb7
Mustafa-Uslu <69405526+Mustafa-Uslu@users.noreply.github.com>
2020-08-13 01:26:00 +0200

    Update TestClass.java
    Githubta degisiklik yaptim

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,7 +3,8 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("Git'ten Merhaba");
-
+		System.out.println("Yerden Merhaba");
+		System.out.println("Github'tan Merhaba");
 	}
 
 }
